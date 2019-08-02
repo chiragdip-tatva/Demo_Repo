b439d7219802a083fcb1ec13ebf5384e9d053e0c
Chiragdip <Chiragdip.israni@tatvasoft.com>
2019-08-02 17:42:15 +0530

    Add extension file and Base response class logic

---
+++ PostList_Demo/PostList_Demo/Extension/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  extension.swift
+//  PostList_Demo
+//
+//  Created by Chiragdip on 02/08/19.
+//  Copyright © 2019 MAC190. All rights reserved.
+//
+
--- PostList_Demo/PostList_Demo/NetworkLayer/BaseResponse.swift
+++ PostList_Demo/PostList_Demo/NetworkLayer/BaseResponse.swift
@@ -6,8 +6,42 @@
 //  Copyright © 2019 MAC190. All rights reserved.
 //
 
-import UIKit
+import SwiftyJSON
 
 class BaseResponse {
-
+ 
+    var message : String?
+    var data : JSON?
+    var error: JSON?
+    
+    
 }
+
+
+//class BaseResponse {
+//
+//    var status: Int = 0
+//    var success: Bool = false
+//    var message: String?
+//    var data: JSON?
+//    var errors: JSON?
+//
+//    init(response:DataResponse<Any>) {
+//        let jsonResult: JSON = JSON(response.result.value!)
+//
+//        self.status  = jsonResult["StatusCode"].intValue
+//        self.success = jsonResult["Status"].boolValue
+//        self.message = jsonResult["Message"].stringValue
+//
+//        if !jsonResult["hits"].isEmpty {
+//            self.data = jsonResult["hits"]
+//            self.success = true
+//        }else{
+//            self.data = jsonResult
+//        }
+//
+//        if !jsonResult["error"].isEmpty {
+//            self.errors = jsonResult["error"]
+//        }
+//    }
+//}
