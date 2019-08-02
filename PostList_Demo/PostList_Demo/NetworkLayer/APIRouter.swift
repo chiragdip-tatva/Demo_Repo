201447b989fc2e0d098bbc2cd828b63c5a34e533
Chiragdip <Chiragdip.israni@tatvasoft.com>
2019-08-02 17:17:40 +0530

    Adding API(Network layer) classes

---
+++ PostList_Demo/PostList_Demo/NetworkLayer/APIRouter.swift
@@ -0,0 +1,13 @@
+//
+//  APIRouter.swift
+//  PostList_Demo
+//
+//  Created by Chiragdip on 02/08/19.
+//  Copyright © 2019 MAC190. All rights reserved.
+//
+
+import UIKit
+
+class APIRouter {
+
+}
