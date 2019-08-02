bf066b2edcf41ac3edda353a002b0fc7ce45f0e7
chiragdip-tatva <53606853+chiragdip-tatva@users.noreply.github.com>
2019-08-02 16:27:59 +0530

    Add files via upload

---
+++ PostList_Demo/PostList_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostList_Demo
+//
+//  Created by Chiragdip on 02/08/19.
+//  Copyright © 2019 MAC190. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
